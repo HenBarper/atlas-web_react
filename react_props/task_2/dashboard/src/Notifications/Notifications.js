ce95d4751af50b546cc2c11f246b16ef94edd6ed
Ben Harper <benjaminitonio@gmail.com>
2024-05-07 13:22:54 -0500

    Task 2. Split the Notifications component
    - Copied over task_1 to task_2
    - Created NotificationItem.js and test
    - Modified Notifications.js and tests

---
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Notifications.css'
+import { getLatestNotification } from '../utils/utils'
+import NotificationItem from './NotificationItem'
+
+class Notification extends Component {
+  render() {
+    return (
+      <div className='Notifications'>
+        <button className='close-button' type='button' onClick={() => console.log('Close button has been clicked')} style={{ display: 'inline', position: 'absolute', top: 1 + 'px', right: 1 + 'px', background: 'none', border: 'none' }} aria-label='Close'><span style={{ fontSize: '24px' }} aria-hidden="true">&times;</span></button>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <NotificationItem type='default' value='New Course Available' />
+          <NotificationItem type='urgent' value='New Resume Available' />
+          <NotificationItem type='urgent' html={getLatestNotification()} />
+        </ul>
+      </div>
+    )
+  }
+}
+
+{/* <li data-priority='default'>New course available</li>
+<li data-priority='urgent'>New resume available</li>
+<li data-priority='urgent' dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li> */}
+
+export default Notification
