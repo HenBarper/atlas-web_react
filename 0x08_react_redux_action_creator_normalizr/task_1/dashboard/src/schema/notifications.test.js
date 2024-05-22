6102b0b93c256960ff41dd971f0d3e91cb05cfe1
Ben Harper <benjaminitonio@gmail.com>
2024-05-22 17:22:58 -0500

    React Redux action Creator Normalizr Task 1. Normalize a nested JSON v1
    Copy over dashboard from the previous task into a task_1 directory at the root of the project
    
    Modify src/schema/notifications.js to set up a schema using Normalizr
    
    You’re going to use schema.Entity to create a 3 of entities.
    
    The first one is an example the task will provide for you.
    
    const user = new schema.Entity("users")
    
    Create a message entity in a variable called message whose key is messages and set the idAttribute to the string guid in the options
    Create a notification entity in a variable called notification whose key is notifications and set the definition of the entity as so:
    author: user
    context: message
    Add a test in schema/notifications.test.js to verify that your normalized data has a correct result array. It should contain:
    
      "5debd76480edafc8af244228"
      "5debd764507712e7a1307303"
      "5debd76444dd4dafea89d53b"
      "5debd76485ee4dfd1284f97b"
      "5debd7644e561e022d66e61a"
      "5debd7644aaed86c97bf9d5e"
      "5debd76413f0d5e5429c28a0"
      "5debd7642e815cd350407777"
      "5debd764c1127bc5a490a4d0"
      "5debd7646ef31e0861ec1cab"
      "5debd764a4f11eabef05a81d"
      "5debd764af0fdd1fc815ad9b"
      "5debd76468cb5b277fd125f4"
      "5debd764de9fa684468cdc0b"
    Add a test to verify that your normalized data has a correct users entity. Test to access the user with the id 5debd764a7c57c7839d722e9. It should return:
    
          age: 25,
          email: "poole.sanders@holberton.nz",
          id: "5debd764a7c57c7839d722e9",
          name: { first: "Poole", last: "Sanders" },
          picture: "http://placehold.it/32x32"
    Add a test to verify that your normalized data has a correct messages entity. Test to access the message with the guid efb6c485-00f7-4fdf-97cc-5e12d14d6c41. It should return:
    
          guid: "efb6c485-00f7-4fdf-97cc-5e12d14d6c41",
          isRead: false,
          type: "default",
          value: "Cursus risus at ultrices mi."
    Add a test to verify that your normalized data has a correct notifications entity. Test to access the notification with the id 5debd7642e815cd350407777. It should return:
    
          author: "5debd764f8452ef92346c772",
          context: "3068c575-d619-40af-bf12-dece1ee18dd3",
          id: "5debd7642e815cd350407777"

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.test.js
@@ -1,9 +1,7 @@
-import { getAllNotificationsByUser } from './notifications';
+import { getAllNotificationsByUser, normalizedData } from './notifications';
 
 describe('getAllNotificationsByUser', () => {
   test('should return the correct notifications for user id 5debd764a7c57c7839d722e9', () => {
-    console.log(getAllNotificationsByUser); // Check if the function is imported correctly
-
     const userId = '5debd764a7c57c7839d722e9';
     const expectedNotifications = [
       {
@@ -22,5 +20,60 @@ describe('getAllNotificationsByUser', () => {
 
     const result = getAllNotificationsByUser(userId);
     expect(result).toEqual(expect.arrayContaining(expectedNotifications));
+  });
+
+  test('verify that your normalized data has a correct result array', () => {
+    const expectedResultArray = [
+      "5debd76480edafc8af244228",
+      "5debd764507712e7a1307303",
+      "5debd76444dd4dafea89d53b",
+      "5debd76485ee4dfd1284f97b",
+      "5debd7644e561e022d66e61a",
+      "5debd7644aaed86c97bf9d5e",
+      "5debd76413f0d5e5429c28a0",
+      "5debd7642e815cd350407777",
+      "5debd764c1127bc5a490a4d0",
+      "5debd7646ef31e0861ec1cab",
+      "5debd764a4f11eabef05a81d",
+      "5debd764af0fdd1fc815ad9b",
+      "5debd76468cb5b277fd125f4",
+      "5debd764de9fa684468cdc0b"
+    ];
+
+    expect(normalizedData.result).toEqual(expect.arrayContaining(expectedResultArray));
+  });
+
+  test('verify that your normalized data has a correct users entity', () => {
+    const userId = '5debd764a7c57c7839d722e9';
+    const expectedReturn = {
+      age: 25,
+      email: "poole.sanders@holberton.nz",
+      id: "5debd764a7c57c7839d722e9",
+      name: { first: "Poole", last: "Sanders" },
+      picture: "http://placehold.it/32x32"
+    }
+
+    expect(normalizedData.entities.users[userId]).toEqual(expectedReturn);
+  });
+
+  test('verify that your normalized data has a correct messages entity', () => {
+    const guid = 'efb6c485-00f7-4fdf-97cc-5e12d14d6c41';
+    const expectedReturn = {
+      guid: "efb6c485-00f7-4fdf-97cc-5e12d14d6c41",
+      isRead: false,
+      type: "default",
+      value: "Cursus risus at ultrices mi."
+    }
+    expect(normalizedData.entities.messages[guid]).toEqual(expectedReturn);
+  });
+
+  test('verify that your normalized data has a correct notifications entity', () => {
+    const id = '5debd7642e815cd350407777';
+    const expectedReturn = {
+      author: "5debd764f8452ef92346c772",
+      context: "3068c575-d619-40af-bf12-dece1ee18dd3",
+      id: "5debd7642e815cd350407777"
+    }
+    expect(normalizedData.entities.notifications[id]).toEqual(expectedReturn);
   });
 });
